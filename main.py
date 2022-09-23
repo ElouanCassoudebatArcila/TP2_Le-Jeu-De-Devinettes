8ba1f8e62851c96fbbed25ae17c2b95cc9d0ea1e
cassoudebatarcilae <abc>
2022-09-23 16:06:13 -0400

    Indetation

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ print("J’ai choisi un nombre au hasard entre 0 et 100. Vous de le deviner...")
 while int(nb_choisi) != int(nombre):
     nombre = input("Entrez votre essai :")
     nb_essai += 1
-    if int(nombre) > int(nb_choisi):02.1#
-    print("Mauvaise réponse, le nombre est plus petit que l essai")
+    if int(nombre) > int(nb_choisi):
+        print("Mauvaise réponse, le nombre est plus petit que l essai")
 
     elif int(nombre) < int(nb_choisi):
-    print("Mauvaise réponse, le nombre est plus grand que l essai")
+        print("Mauvaise réponse, le nombre est plus grand que l essai")
 
     elif int(nombre) == int(nb_choisi):
-    print("Bravo! Bonne réponse")
-    print("Vous avez réussi en ", nb_essai, "essai(s)")
-    play_again = input("Voulez-vous faire une autre partie (o/n)?")
-    if play_again == ("n"):
-    print("Merci et au revoir…")
-    break
-    elif play_again == ("o"):
+        print("Bravo! Bonne réponse")
+        print("Vous avez réussi en ", nb_essai, "essai(s)")
+        play_again = input("Voulez-vous faire une autre partie (o/n)?")
+        if play_again == ("n"):
+            print("Merci et au revoir…")
+        break
 
 
 
