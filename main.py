2f0c38de9eab88bfb5120abef499431661d18f25
cassoudebatarcilae <abc>
2022-09-30 10:42:38 -0400

    finit

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
-'''
-programme ayant pour but de trouver un nombre choissi de manière aléatoire entre deux bornes choissi par l'utilisateur
-'''
+"""
+Programme par Elouan Cassoudebat--Arcila, Groupe 407
+programme ayant pour but de trouver un nombre choisi de manière aléatoire choisi par l'ordinateur entre deux bornes
+choissi par l'utilisateur
+"""
 
 
 from random import randint
 
-def  get_int_input(phrase:str)->int:
+
+def get_int_input(phrase: str) -> int:
+    """
+    s'assure que le nombre du joueur soit bien un nombre
+    """
     try:
         nombre = int(input(phrase))
     except ValueError:
         nombre = get_int_input(phrase)
     return nombre
 
+
 def play_game():
-    nb_essai = 0   #le nombre d'essai
-    nb_un = input("Choissisez la borne la plus petite:\n -->")  #la borne une (plus petite)
-    nb_deux = input("Choissisez la borne la plus grande:\n -->")  #la borne deux (plus grande)
-    nb_choisi = randint(int(nb_un), int(nb_deux))   #le nombre aléatoire a trouver
-    print (nb_choisi)
-    nombre = 1000000  #le nombre a deviner
-    print("J’ai choisi un nombre au hasard entre" ,nb_un, "et" ,nb_deux, "Vous de le deviner...")
-    while int(nb_choisi) != int(nombre := get_int_input("Entrez votre essai\n -->")):
+    """
+    Choisi le nombre aléatoire et permet au joueur de le deviner (Le jeu)
+    """
+    nb_essai = 0
+    nb_un = input("Choissisez la borne la plus petite:\n -->")  # la borne une (plus petite)
+    nb_deux = input("Choissisez la borne la plus grande:\n -->")  # la borne deux (plus grande)
+    nb_choisi = randint(int(nb_un), int(nb_deux))  # le nombre aléatoire a trouver
+    print("J’ai choisi un nombre au hasard entre", nb_un, "et", nb_deux, "Vous de le deviner...")
+    while nb_choisi != (nombre_joueur := get_int_input("Entrez votre essai\n -->")):
         nb_essai += 1
-        if int(nombre) > int(nb_choisi):
+        if nombre_joueur > nb_choisi:
             print("Mauvaise réponse, le nombre est plus petit que l\'essai")
 
-        elif int(nombre) < int(nb_choisi):
+        elif nombre_joueur < nb_choisi:  # previent l'utilisateur que le nombre est trop petit
             print("Mauvaise réponse, le nombre est plus grand que l\'essai")
 
-        if int(nombre) == int(nb_choisi):
-            print("Bravo! Bonne réponse")
-            print("Vous avez réussi en ", nb_essai, "essai(s)")
+    nb_essai += 1  # previent l'utilisateur que le nombre est le bon
+    print("Bravo! Bonne réponse")
+    print("Vous avez réussi en ", nb_essai, "essai(s)")
 
 
 while True:
     play = input("\n Voulez-vous jouer (o/n)?\n -->")
-    if play == ("n"):
-            print("ah ok...")
-            break
-    if play == ("o"):
-            play_game()
+    if play == "n":
+        print("ah ok...")
+        break
+    if play == "o":
+        play_game()
